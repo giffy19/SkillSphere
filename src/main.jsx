77160b743f4119725eefb9a0eeed255feda3c755
Giflin Godshia <giflingodshia19@gmail.com>
2025-09-08 18:47:12 +0530

    Update SkillSphere for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./App.css";
-
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter } from "react-router-dom";  // import BrowserRouter
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/SkillSphere">  {/* 👈 important */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: [".."],
-    },
-  },
-  build: {
-    rollupOptions: {},
-  },
-  preview: {
-    port: 4173,
-    strictPort: true,
-  },
-  base: '/SkillSphere/'
+  base: "/SkillSphere/",  // 👈 This is critical for GitHub Pages
 });
