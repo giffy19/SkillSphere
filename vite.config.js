e5e6703d162a18a25bc196620c3e555a5adfe692
Giflin Godshia <giflingodshia19@gmail.com>
2025-09-15 16:18:30 +0530

    Fix build for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // important for Vercel (keeps assets relative)
+  base: "/", // ✅ must be root for Vercel
 });
